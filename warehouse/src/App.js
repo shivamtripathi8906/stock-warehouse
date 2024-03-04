b9c02545af29afb5615978654a3dfe9e883f617f
shivamtripathi8906 <shivamtripathi159@gmail.com>
2024-03-04 19:51:04 +0530

    Routing Done

--- warehouse/src/App.js
+++ warehouse/src/App.js
@@ -1,9 +1,15 @@
+import { Route , Routes } from 'react-router';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
+
+
   return (
     <div className="App">
-      App
+      <Routes>
+        <Route exact path = "/home" Component={Home}/>
+      </Routes>
     </div>
   );
 }
